dfb20f13b18ef91dd09493422332e2f795617f63
hchangjae <hcj@payw.org>
2020-01-15 10:28:16 +0900

    First build

--- src/main/java/com/example/demo/SpringBootGradleApplication.java
+++ src/main/java/com/example/demo/SpringBootGradleApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootGradleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootGradleApplication.class, args);
+            SpringApplication.run(SpringBootGradleApplication.class, args);
+
     }
 
 }
